e5d0287ea84195f8aeacc274061c78732c1168a9
Ahdad0 <abderrahimahdadabdo@gmail.com>
2023-09-12 13:17:12 +0100

    updated

--- execve.c
+++ execve.c
@@ -1,3 +1,36 @@
 #include "shell.h"
 
+int execute(char **str, char *av, char **en)
+{
+	pid_t piid;
+	int statu;
 
+	if (access(str[0], X_OK) == 0)
+	{
+		piid = fork();
+
+		if (piid == -1)
+		{
+			perror("Fork");
+			exit (EXIT_FAILURE);
+		}
+		if (piid == 0)
+		{
+			if (execve(str[0], str, en) == -1)
+			{
+				perror(av);
+				return (EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(piid, &statu, WUNTRACED);
+		}
+	}
+	else
+	{
+		perror(str[0]);
+	}
+
+	return (1);
+}
