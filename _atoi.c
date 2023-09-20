1ee91cce096a8cfda7a77511a0a504e9073570d5
faouziaen <faouziaennaama@gmail.com>
2023-09-20 15:42:45 +0100

    betty

--- _atoi.c
+++ _atoi.c
@@ -1,29 +1,33 @@
 #include "shell.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string or if there is an error, converted number otherwise
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer,
+ * or -1 on error or if no numbers are in the string.
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-
-    if (*s == '-')
-    {
-        sign = -1;
-        s++;
-    }
-    while (*s)
-    {
-        if (*s >= '0' && *s <= '9')
-        {
-            result = result * 10 + (*s - '0');
-            s++;
-        }
-        else
-            return -1;
-    }
-    return sign * result;
+	int sign = 1;
+	int result = 0;
+	
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+			s++;
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (sign * result);
 }
